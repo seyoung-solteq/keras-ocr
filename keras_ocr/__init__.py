92a0cb9ec446d3aab4d09e653c45a8c1447bd903
Seyoung Park <seyoung.park@solteq.com>
2021-03-01 14:39:00 +0200

    (WIP) Rewrite setup.py

--- keras_ocr/__init__.py
+++ keras_ocr/__init__.py
@@ -1,4 +1,5 @@
-from . import (detection, recognition, tools, data_generation, pipeline, evaluation, datasets)
+# from . import (detection, recognition, tools, data_generation, pipeline, evaluation, datasets)
+from . import (recognition, tools)
 
 from ._version import get_versions
 __version__ = get_versions()['version']
--- setup.py
+++ setup.py
@@ -1,8 +1,45 @@
-# -*- coding: utf-8 -*-
-
+import pathlib
 from setuptools import setup
-import versioneer
 
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
 
-setup(version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass())
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="keras-ocr",
+    version="1.0.0",
+    description="A packaged and flexible version of the CRAFT text detector and Keras CRNN recognition model.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="",
+    author="Fausto Morales, Seyoung Park",
+    author_email="seyoung.arts.park@protonmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["keras_ocr"],
+    include_package_data=True,
+    install_requires=[
+      'essential_generators',
+      'tqdm',
+      'imgaug',
+      'validators',
+      'fonttools',
+      'editdistance',
+      'pyclipper',
+      'shapely',
+      # 'efficientnet==1.0.0',    # Needed for detection.py
+      'keras',
+      'numpy',
+      'yapf',
+      'tensorflow==2.3.1',
+      'h5py<=2.10.0',
+      'Pillow==7.1.0',
+    ],
+)
